370b902a87315311eafdca9e665f303634e41f9b
Anna Rafaela Bispo de Santana <anna.santana@compasso.com.br>
2023-06-07 11:33:31 -0300

    create endpoint list medicos

--- src/main/java/com/med/voll/api/controller/MedicoController.java
+++ src/main/java/com/med/voll/api/controller/MedicoController.java
@@ -1,12 +1,16 @@
 package com.med.voll.api.controller;
 
 import com.med.voll.api.medico.DadosCadastroMedico;
+import com.med.voll.api.medico.ListarMedicoDto;
 import com.med.voll.api.medico.Medico;
 import com.med.voll.api.medico.MedicoRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/medicos")
@@ -22,4 +26,10 @@ public class MedicoController {
         medicoRepository.save(medico);
     }
 
+    @GetMapping
+    public List<ListarMedicoDto> index(){
+        List<Medico> medicos =  medicoRepository.findAll();
+
+        return medicos.stream().map(ListarMedicoDto::new).toList();
+    }
 }
