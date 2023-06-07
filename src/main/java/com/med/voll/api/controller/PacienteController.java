5d48aea15373a97e6d54621c116f600a8f59c54b
Anna Rafaela Bispo de Santana <anna.santana@compasso.com.br>
2023-06-07 10:55:02 -0300

    add endpoint POST create Paciente

--- src/main/java/com/med/voll/api/controller/PacienteController.java
+++ src/main/java/com/med/voll/api/controller/PacienteController.java
@@ -1,6 +1,12 @@
 package com.med.voll.api.controller;
 
+import com.med.voll.api.medico.Medico;
 import com.med.voll.api.paciente.DadosCadastroPaciente;
+import com.med.voll.api.paciente.Paciente;
+import com.med.voll.api.paciente.PacienteRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("pacientes")
 public class PacienteController {
 
+    @Autowired
+    private PacienteRepository pacienteRepository;
+
     @PostMapping
-    public void cadastrar(@RequestBody DadosCadastroPaciente dados) {
-        System.out.println("dados recebido: " +dados);
+    @Transactional
+    public void create(@RequestBody @Valid DadosCadastroPaciente data){
+        Paciente medico = new Paciente(data);
+        pacienteRepository.save(medico);
     }
 
 }
