06c5d4b0349aba0a6213871ed3346d45dba89bed
Anna Rafaela Bispo de Santana <anna.santana@compasso.com.br>
2023-06-13 15:54:48 -0300

    refactor Controller Medico

--- src/main/java/com/med/voll/api/controller/MedicoController.java
+++ src/main/java/com/med/voll/api/controller/MedicoController.java
@@ -7,9 +7,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -23,29 +26,44 @@ public class MedicoController {
 
     @PostMapping
     @Transactional
-    public void create(@RequestBody @Valid DadosCadastroMedico data){
+    public ResponseEntity<MedicoDto> create(@RequestBody @Valid DadosCadastroMedico data, UriComponentsBuilder uriComponentsBuilder){
         Medico medico = new Medico(data);
         medicoRepository.save(medico);
+        URI uri = uriComponentsBuilder.path("/medicos/{id}").buildAndExpand(medico.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(new MedicoDto(medico));
     }
 
     @GetMapping
-    public Page<MedicoDto> index(@PageableDefault(size = 2, sort = {"nome"}) Pageable pagination){
-        return medicoRepository.findAllByAtivoTrue(pagination).map(MedicoDto::new);
+    public ResponseEntity<Page<MedicoDto>> index(@PageableDefault(size = 2, sort = {"nome"}) Pageable pagination){
+        Page page = medicoRepository.findAllByAtivoTrue(pagination).map(MedicoDto::new);
+
+        return ResponseEntity.ok(page);
     }
     @PutMapping("/{id}")
     @Transactional
-    public void update(@PathVariable Long id, @RequestBody @Valid DadosUpdateMedicoDto data){
+    public ResponseEntity<MedicoDto> update(@PathVariable Long id, @RequestBody @Valid DadosUpdateMedicoDto data){
         Medico medico = medicoRepository.getReferenceById(id);
         medico.update(data);
+
+        return ResponseEntity.ok(new MedicoDto(medico));
     }
 
     @DeleteMapping("/{id}")
     @Transactional
-    @ResponseStatus(code = HttpStatus.NO_CONTENT)
-    public void delete(@PathVariable Long id){
+    public ResponseEntity delete(@PathVariable Long id){
         Medico medico  = medicoRepository.getReferenceById(id);
         medico.destroyLogic(id);
 
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/{id}")
+    @Transactional
+    public ResponseEntity show(@PathVariable Long id){
+        Medico medico  = medicoRepository.getReferenceById(id);
+
+        return ResponseEntity.ok(new MedicoDto(medico));
     }
 
 
--- src/main/java/com/med/voll/api/medico/MedicoDto.java
+++ src/main/java/com/med/voll/api/medico/MedicoDto.java
@@ -2,9 +2,9 @@ package com.med.voll.api.medico;
 
 import com.med.voll.api.endereco.Endereco;
 
-public record MedicoDto(Long Id, String nome, String email, String crm, Especialidade especialidade) {
+public record MedicoDto(Long Id, String nome, String email,String telefone, String crm, Especialidade especialidade, Endereco endereco) {
     public MedicoDto(Medico medico) {
-       this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade());
+       this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getTelefone(), medico.getCrm(), medico.getEspecialidade(), medico.getEndereco());
     }
 
 
