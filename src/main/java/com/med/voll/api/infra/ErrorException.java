ed285770f61bcd3dd2d33aa727207c9c936d8ba6
Anna Rafaela Bispo de Santana <anna.santana@compasso.com.br>
2023-06-13 16:49:57 -0300

    create class ErrorException custom return errors api

---
+++ src/main/java/com/med/voll/api/infra/ErrorException.java
@@ -0,0 +1,33 @@
+package com.med.voll.api.infra;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+
+@RestControllerAdvice
+public class ErrorException {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity handleError404 () {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity handleError400 (MethodArgumentNotValidException e) {
+        var errors = e.getFieldErrors().stream().map(ErrorDto::new).toList();
+
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+
+    private record ErrorDto(String field, String message){
+        public ErrorDto(FieldError erro) {
+            this(erro.getField(), erro.getDefaultMessage());
+        }
+    }
+}
